28f8b7383bdc80d40dcb22d646a905d844d30c4d
palloncino <antonio.guiotto.dev@gmail.com>
2024-03-03 14:26:40 +0100

    3 March, 2024

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', '' );
+define( 'DB_NAME', 'LOCAL_DALLA_MORA' );
 
 /** Database username */
-define( 'DB_USER', '' );
+define( 'DB_USER', 'root' );
 
 /** Database password */
 define( 'DB_PASSWORD', '' );
 
 /** Database hostname */
-define( 'DB_HOST', '' );
+define( 'DB_HOST', 'localhost' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
